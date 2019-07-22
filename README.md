17314b7ea4e586a5e6ca3be626c6ff98d1ccc68b
moquet <robin.m07@hotmail.fr>
2019-07-22 10:53:06 +0200

    modification du readme

--- README.md
+++ README.md
@@ -5,15 +5,18 @@
 Un peu de text ;)
 
 ## Installation :
+Récupérer le projet via git :
+```
+$ git clone https://github.com/robinmoquet/labo_01.git
+```
 
-[git] : à venir
 
-1. Installation des dépendances php, via composer :
+1/ Installation des dépendances php, via composer :
 
 ```
 $ composer update
 ```
-2. Installation des dépendances javascript, via npm :
+2/ Installation des dépendances javascript, via npm :
 
 ```
 $ npm install
@@ -23,4 +26,20 @@ $ npm install
 $ yarn install
 ```
 
+3/ Installer la dépendance mercure, dans une dossier mercure.
+    Telecharger, puis déziper le tar.gz qui correspond a votre environnement, trouvé
+    sur le release suivant [Release Mercure](https://github.com/dunglas/mercure/releases)
 
+4/ Modifier la connection a la base de donné dans le ".env",
+* creation de la base de donné : ``php bin/console doctrine:database:create``
+* creation des migrations : ``php bin/console make:migration``
+* mise à jour de la base donné : ``php bin/console doctrine:migrations:migrate``
+
+### Lancer
+
+1/ Démarer le serveur php : ``php bin/console server:run``
+2/ Lancer la compilation des assets : ``yarn encore dev --watch``, ou en prod ``yarn encore production``
+3/ Lancer le hub mercure : 
+```
+JWT_KEY='aVerySecretKey' ADDR='localhost:3000' ALLOW_ANONYMOUS=1 CORS_ALLOWED_ORIGINS=* ./mercure
+```
---
+++ src/Migrations/Version20190722083757.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190722083757 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, sender_id INT NOT NULL, receiver_id INT NOT NULL, content LONGTEXT DEFAULT NULL, status VARCHAR(50) NOT NULL, create_at DATETIME NOT NULL, INDEX IDX_B6BD307FF624B39D (sender_id), INDEX IDX_B6BD307FCD53EDB6 (receiver_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FF624B39D FOREIGN KEY (sender_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FCD53EDB6 FOREIGN KEY (receiver_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FF624B39D');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FCD53EDB6');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE user');
+    }
+}
